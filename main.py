94c8d85f0f533036377db8e125f6193e56eba982
Egor Zhukov <bit.zhukov@gmail.com>
2022-03-16 11:40:29 +0300

    16.03.22 finished

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+import gym
+from tensorboardX import SummaryWriter
+import Agent
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+ENV_NAME = "FrozenLake-v0"
+GAMMA = 0.9
+TEST_EPISODES = 20
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    test_env = gym.make(ENV_NAME)
+    agent = Agent(env_name=ENV_NAME, gamma=GAMMA)
+    writer = SummaryWriter(comment="-v-iteration")
+    iter_no = 0
+    best_reward = 0.0
+    while True:
+        iter_no += 1
+        agent.play_n_random_steps(100)
+        agent.value_iteration()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        reward = 0.0
+        for _ in range(TEST_EPISODES):
+            reward += agent.play_episode(test_env)
+        reward /= TEST_EPISODES
+        writer.add_scalar("reward", reward, iter_no)
+        if reward > best_reward:
+            print("Best reward update %.3f -> %.3f" % (best_reward, reward))
+            best_reward = reward
+        if reward > 0.80:
+            print("Solved in %d iterations!" % iter_no)
+            break
+    writer.close()
+
+
